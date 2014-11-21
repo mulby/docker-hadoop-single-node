e42389d91c5e7114c47a455c3b7a595b301e4b4c
pchiu33 <philippe.chiu@gmail.com>
2014-11-21 13:20:30 +0100

    Added /data creation in local HDFS

--- services/start-edx-analytics.sh
+++ services/start-edx-analytics.sh
@@ -4,6 +4,8 @@
 su hduser -c "$HADOOP_HOME/sbin/start-dfs.sh"
 su hduser -c "$HADOOP_HOME/sbin/start-yarn.sh"
 
+sudo -u hduser /usr/local/hadoop/bin/hdfs dfs -mkdir /data
+
 cd /opt/edx-analytics-data-api ; ./manage.py runserver 0.0.0.0:8000 &
 
 service mongodb start
